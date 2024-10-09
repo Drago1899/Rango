bb493d528863bc0f8909a99a6a686992425bf7d7
CocoHoshi <alex_2505@live.com>
2024-10-09 13:34:08 -0600

    menu

---
+++ RangoMidSem/Assets/Scripts/Photon0.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Photon0 : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+
+    {
+
+        PhotonNetwork.ConnectUsingSettings();
+
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        print("Se ha conectado al master");
+        PhotonNetwork.JoinLobby();
+
+    }
+    public override void OnJoinedLobby()
+    {
+        PhotonNetwork.LoadLevel("Menu");
+        Debug.Log("Lobby");
+
+
+    }
+
+}
---
+++ RangoMidSem/Assets/Scripts/Photon1.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+using Photon.Realtime;
+
+
+
+
+
+public class Photon1 : MonoBehaviourPunCallbacks
+
+{
+    [SerializeField] TMP_InputField NickName;
+    [SerializeField] TMP_Text NoNickName;
+    [SerializeField] string Room;
+    void Start()
+    {
+     
+        NoNickName.text = "";
+        
+    }
+    public override void OnJoinedRoom()
+    {
+        print("Se entró al room");
+        
+        PhotonNetwork.LoadLevel("Juego");
+    }
+
+
+    RoomOptions newRoomInfo()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 4;
+        roomOptions.IsOpen = true;
+        roomOptions.IsVisible = true;
+        
+
+        return roomOptions;
+    }
+  
+
+    public void createRoom()
+    {
+        if (NickName.text == "")
+        {
+            print("Necesitas un nombre");
+            NoNickName.text = "Necesitas un nombre";
+            return;
+        }
+        PhotonNetwork.NickName = NickName.text;
+        PhotonNetwork.CreateRoom(Room, newRoomInfo(), null);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Cerrando");
+    }
+
+}
